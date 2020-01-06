644cc5ea18759dd8bbec1e65e03645125e2abc67
ksh44820@likelion.org <ksh44820@likelion.org>
2020-01-06 12:09:04 +0900

    Component performance improvement

--- src/App.js
+++ src/App.js
@@ -3,24 +3,41 @@ import TodoTemplate from './components/TodoTemplate'
 import TodoInsert from './components/TodoInsert'
 import TodoList from './components/TodoList'
 
-function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: "리액트",
-      checked: true
-    },
-    {
-      id: 2,
-      text: "공부",
+function createBulkTodos() {
+  const array =[];
+  for( let i = 0 ; i<=2000; i++){
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
       checked: false
-    },
-    {
-      id: 3,
-      text: "운동",
-      checked: true
-    },
-  ])
+    });
+  };
+  return array;
+}
+
+function App() {
+  const [todos, setTodos] = useState(createBulkTodos);
+
+  // const [todos, setTodos] = useState(
+  //   [
+  //   {
+  //     id: 1,
+  //     text: "리액트",
+  //     checked: true
+  //   },
+  //   {
+  //     id: 2,
+  //     text: "공부",
+  //     checked: false
+  //   },
+  //   {
+  //     id: 3,
+  //     text: "운동",
+  //     checked: true
+  //   },
+  // ]
+  //)
+
   // 지역 변수로 사용하기 위해 useRef hook 사용
   const nextId = useRef(4);
   
@@ -32,25 +49,34 @@ function App() {
         text,
         checked: false
       }
-      setTodos(todos.concat(todo));
+      // setTodos(todos.concat(todo));
+      // useState 함수형 업데이트
+      setTodos(todos => todos.concat(todo));
       nextId.current += 1;
-  }, [todos])
+  }, [])
 
   // 글 삭제 함수
   const onRemove = useCallback(
     id=>{
-      setTodos(todos.filter( todo => id !== todo.id))
-    }, [todos]
+      // setTodos(todos.filter( todo => id !== todo.id))
+      // useState 함수형 업데이트
+      setTodos(todos => todos.filter( todo => id !== todo.id))
+    }, []
   )
 
   // check 함수
   const onToggle = useCallback(
     id=>{
-      setTodos(todos.map(todo=>
+      // setTodos(todos.map(todo=>
+      //   // 선택한 todo의 checked 값만 반대로 설정
+      //   id === todo.id ? { ...todo, checked: !todo.checked} : todo
+      // ))
+      // useState 함수형 업데이트
+      setTodos(todos => todos.map(todo=>
         // 선택한 todo의 checked 값만 반대로 설정
         id === todo.id ? { ...todo, checked: !todo.checked} : todo
       ))
-    }, [todos]
+    }, []
   )
 
   return (
